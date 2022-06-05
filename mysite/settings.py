a1e03af2889bd905577fdeb69a332b0387816813
babakshirin <babakshirin@yahoo.com>
2022-06-05 16:02:41 +0430

    0315

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,18 +113,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR / 'ststic'
+STATIC_URL = '/static/'
+#STATIC_ROOT=BASE_DIR /'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
